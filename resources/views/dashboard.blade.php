0fea3a184ca1245986897da441da5909f6f0c2be
Daniel Billing <daniel@infihex.com>
2019-12-08 20:39:17 +0100

    Updated dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,16 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
-                </div>
-            @endif
             <div class="card">
-                <div class="card-header">{{ __('Du er en del av disse:') }}</div>
+                <div class="card-header">{{ __('Du er medlem av disse:') }}</div>
                 <div class="card-body">
                     @foreach (Auth::user()->condos as $condo)
-                        <p><a href="{{ route('c-show', $condo->orgnr) }}">{{ $condo->orgnr ?? '' }} &middot; {{ $condo->name ?? '' }}</a></p>
+                        <p class="mb-2"><a href="{{ route('c-show', $condo->orgnr) }}">{{ $condo->orgnr ?? '' }} &middot; {{ $condo->name ?? '' }}</a></p>
                     @endforeach
                 </div>
             </div>
