de9b03843f7b54c9704e01be481c04e905b9c4c3
Daniel Billing <daniel@infihex.com>
2019-12-08 19:18:41 +0100

    Added UUID to UserSeeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -8,30 +8,35 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         User::create([
-            'email'         => 'test@infihex.com',
-            'password'      => '12345678', // Den hash'r automatisk
-            'name'          => 'John MarkveienAdmin',
-        ]);
+            'email' => 'test@infihex.com',
+            'password' => Hash::make('12345678'), // Den hash'r automatisk
+            'name' => 'John MarkveienAdmin',
+            'uuid' => User::generateUUID(),
+        ])->condos()->attach(1);
         if (Config::get('app.debug')) {
             User::create([
-                'email'         => 'test2@infihex.com',
-                'password'      => '12345678', // Den hash'r automatisk
-                'name'          => 'John Markveien26A',
+                'email' => 'test2@infihex.com',
+                'password' => Hash::make('12345678'), // Den hash'r automatisk
+                'name' => 'John Markveien26A',
+                'uuid' => User::generateUUID(),
             ]);
             User::create([
-                'email'         => 'test3@infihex.com',
-                'password'      => '12345678', // Den hash'r automatisk
-                'name'          => 'John Markveien26B',
+                'email' => 'test3@infihex.com',
+                'password' => Hash::make('12345678'), // Den hash'r automatisk
+                'name' => 'John Markveien26B',
+                'uuid' => User::generateUUID(),
             ]);
             User::create([
-                'email'         => 'test4@infihex.com',
-                'password'      => '12345678', // Den hash'r automatisk
-                'name'          => 'John Markveien26C',
+                'email' => 'test4@infihex.com',
+                'password'      => Hash::make('12345678'), // Den hash'r automatisk
+                'name' => 'John Markveien26C',
+                'uuid' => User::generateUUID(),
             ]);
             User::create([
-                'email'         => 'test5@infihex.com',
-                'password'      => '12345678', // Den hash'r automatisk
-                'name'          => 'John Markveien26D',
+                'email' => 'test5@infihex.com',
+                'password' => Hash::make('12345678'), // Den hash'r automatisk
+                'name' => 'John Markveien26D',
+                'uuid' => User::generateUUID(),
             ]);
         }
     }
