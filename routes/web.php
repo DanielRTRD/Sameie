88cc44bb55218ef0a05bb8f03acb09e1162783b2
Daniel Billing <daniel@infihex.com>
2019-12-08 19:18:11 +0100

    Added update command to reset

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,8 @@ if (Config::get('app.debug')) {
         Artisan::call('migrate:reset');
         Artisan::call('migrate');
         Artisan::call('db:seed');
-        return redirect('/loginn')->with('status', 'DB has been reset.');
+        Artisan::call('sameie:update');
+        return redirect('/')->with('status', 'DB has been reset.');
     });
     Route::get('/test', function () {
         App::abort(404);
